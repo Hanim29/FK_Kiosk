5787270c60ffcf974dcbfc861c217bb46285d12b
Aishah Johan <sitiaishahjohan9@gmail.com>
2023-12-18 23:18:13 +0800

    bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Kiosk Application</title>
     <!-- Include Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="/bootstrap-5.3.2-dist/css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom Styles -->
     <style>
@@ -58,8 +58,7 @@
     </div>
 
     <!-- Include Bootstrap JS and Popper.js -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="/bootstrap-5.3.2-dist/js/bootstrap.js"></script>
+    
 </body>
 </html>
