67036527f1b7a4821e7ea41b6a210e8eb796ce27
Hanims <hnmblqs@gmail.com>
2023-12-18 15:02:34 +0800

    tryController

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class paymentController extends Controller
 {
-    //
+    public function index()
+	{
+		$payment = Payment::all();
+		return view('PaymentInterface', [
+			'payment' => $payment,
+		]);
+	}
 }
+
